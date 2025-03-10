728497dbeac550d9f4ef7390e86a6edad389cc6b
Ghassen kachroudi <ghassen.kachroudi10@gmail.com>
2025-03-10 11:13:36 +0100

    resolution du conflit

--- projet.py
+++ projet.py
@@ -1,3 +1,4 @@
+
 def somme (T):
       S=0
   for t in T
@@ -6,4 +7,12 @@ def somme (T):
  Data=[1,3,5]
 Som=Sum(Data)
 print('la somme est : ', Som)  
+
+Data=[1,3,5]
+if Data :
+      print('la somme est :' sum (Data))
+  print('le min est :', min(data)) 
+  print('le max est :',max(data)) 
+else:
+  print('dossier vide') 
 
